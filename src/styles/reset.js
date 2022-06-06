68512742501914ad08dba60104deffe3b57fe7f2
Jaeuk Lim <applewodnr@naver.com>
2022-06-06 17:41:07 +0900

    style: reset css 수정
    - a 태그의 밑줄을 제거하기 위해, text-decoration:none 추가

--- src/styles/reset.js
+++ src/styles/reset.js
@@ -1,15 +1,19 @@
 import { css } from 'styled-components';
 
 const resetCSS = css`
-	* {
-		box-sizing: border-box;
-	}
+    * {
+        box-sizing: border-box;
+    }
 
-	html,
-	body {
-		margin: 0;
-		padding: 0;
-	}
+    html,
+    body {
+        margin: 0;
+        padding: 0;
+    }
+
+    a {
+        text-decoration: none;
+    }
 `;
 
 export default resetCSS;
